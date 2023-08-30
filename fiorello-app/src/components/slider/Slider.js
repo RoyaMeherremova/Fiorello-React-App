e84b4b1c3a4f0841742cf730756a2ae8bc9ec117
unknown <royaam@code.edu.az>
2023-08-30 16:39:45 +0400

    Slider section is ready

---
+++ fiorello-app/src/components/slider/Slider.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import '../../assets/scss/_slider.scss'
+import slider1 from '../../assets/images/slider/h3-slider-background.jpg'
+import slider2 from '../../assets/images/slider/h3-slider-background-2.jpg'
+import slider3 from '../../assets/images/slider/h3-slider-background-3.jpg'
+import sliderLogo from '../../assets/images/slider/h2-sign-img.png'
+
+
+
+function Slider() {
+    return (
+        <section id='slider'>
+            <Carousel>
+                <Carousel.Item>
+                    <img className='slide' src={slider1} />
+                    <Carousel.Caption>
+                        <div className='slide-content'>
+                            <h1>Send <span>flowers</span> like</h1>
+                            <h1>you mean it</h1>
+                            <p>Where flowers are our inspiration to create lasting memories. Whatever the occasion, our flowers will
+                                make it special cursus a sit amet mauris. </p>
+                            <img src={sliderLogo} />
+
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='slide' src={slider2} />
+                    <Carousel.Caption>
+                        <div className='slide-content'>
+                            <h1>Send <span>flowers</span> like</h1>
+                            <h1>you mean it</h1>
+                            <p>Where flowers are our inspiration to create lasting memories. Whatever the occasion, our flowers will
+                                make it special cursus a sit amet mauris. </p>
+                            <img src={sliderLogo} />
+
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className='slide' src={slider3} />
+                    <Carousel.Caption>
+                        <div className='slide-content'>
+                            <h1>Send <span>flowers</span> like</h1>
+                            <h1>you mean it</h1>
+                            <p>Where flowers are our inspiration to create lasting memories. Whatever the occasion, our flowers will
+                                make it special cursus a sit amet mauris. </p>
+                            <img src={sliderLogo} />
+
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </section>
+
+    );
+}
+
+export default Slider;
+
+
--- fiorello-app/src/pages/Home.js
+++ fiorello-app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../assets/scss/_home.scss'
+import Slider from '../components/slider/Slider'
 
 function Home() {
     return (
         <div>
             <main>
-
+                <Slider />
             </main>
         </div>
     )
